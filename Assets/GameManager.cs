6d0b1e1f10cec1f4f30f131c6eac032eea40fa43
Karen <photo.karen@gmail.com>
2020-05-06 11:03:52 -0700

    5-6 class

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(spawnTargets());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnTargets()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            int targetIndex = Random.Range(0, targets.Count);
+            Instantiate(targets[targetIndex]);
+        }
+        
+    }
+}
